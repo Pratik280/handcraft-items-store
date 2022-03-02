310cc3646621ce61bbbc480eb21f1427489d8b27
Pratik280 <pratikchandlekar280@gmail.com>
2022-03-02 10:59:29 +0530

    color palette added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,5 +3,14 @@ module.exports = {
     "./public/**/*.html",
     "./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}",
   ],
-  // more options here
+  theme: {
+    extend: {
+      colors: {
+        "clr-darker": "hsla(215, 25%, 23%, 1)",
+        "clr-dark": "hsla(215, 25%, 27%, 1)",
+        "clr-accent": "hsla(176, 100%, 39%, 1)",
+        "clr-light": "hsla(48, 100%, 92%, 1)",
+      },
+    },
+  },
 };
